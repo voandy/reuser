369e98a1ada36fdaa3b0532d7fea6456e9e67155
Shaun <shaunsoongdidi@gmail.com>
2019-03-20 10:36:19 +1100

    tutorial 3, exercises (1&2). I havent complete ex1 cuz im lazy

---
+++ tut3/ex2.js
@@ -0,0 +1,7 @@
+function alertName() {
+
+    var first = document.getElementById("firstname").value;
+    var last  = document.getElementById("lastname").value;
+
+    alert("Hello There, " + first + " " + last);
+}
