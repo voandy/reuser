a6d495a5920a21cdbb9b270f1250649509668a5a
Andy Vo <andyvo@Andys-MacBook.local>
2019-03-27 12:27:08 +1100

    removed tutorial files

--- tut3/ex2.js
+++
@@ -1,7 +0,0 @@
-function alertName() {
-
-    var first = document.getElementById("firstname").value;
-    var last  = document.getElementById("lastname").value;
-
-    alert("Hello There, " + first + " " + last);
-}
--- tut3/exercise2_sugianto.js
+++
@@ -1,5 +0,0 @@
-function getFormValue() {
-    var firstName = document.forms["formname"]["firstname"].value;
-    var lastName = document.forms["formname"]["lastname"].value;
-    alert("Hi " + firstName + " " + lastName);
-}
