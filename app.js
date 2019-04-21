4a0ba85ea01fa96a2c692635c4cd199f914c66b8
Andy Vo <andy@voandy.com>
2019-04-21 22:25:41 +1000

    Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 7900;
-
-
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-
-
 // setup MongoDB Atlas
 require('./models/db.js');
-
 // routes setup
 var routes = require('./routes/routes.js');
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+app.use(bodyParser.json());
 app.use('/',routes);
 
 app.get('/', function(req,res){
