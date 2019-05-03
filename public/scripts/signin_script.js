c620074db07ffe8b74890449c0e8a923f1bbc5f6
Ze Shaun Soong <z.soong@student.unimelb.edu.au>
2019-05-03 17:48:28 +1000

    test html included (unfinished sign uppage)

--- public/scripts/signin_script.js
+++ public/scripts/signin_script.js
@@ -1,57 +1,93 @@
-// email validator
-$(document).ready(function () {
-    $("#email").focusout(function () {
-        email_validator();
-    })
+// $(document).ready(function () {
 
-    $("#pwd").focusout(function () {
-        password_validator();
-    })
+//     $("#email").focusout(function () {
+//         email_validator();
+//     })
 
-    $("#pwdConfirm").focusout(function () {
-        password_length_validator();
-    })
+//     $("#pwd").focusout(function () {
+//         password_validator();
+//     })
 
-    function email_validator() {
-        const pattern   = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-        var email       = $('#email').val();
+//     $("#pwdConfirm").focusout(function () {
+//         password_length_validator();
+//     })
 
-        if (pattern.test(email)) {
-            $('#invalidEmail').css('display', 'none');
-            $('#email').removeClass('is-invalid');
-        }
-        else {
-            $('#invalidEmail').css('display', 'block');
-            $('#email').addClass('is-invalid');
-        }
-    }
 
-    function password_validator() {
-        const min   = 8;
-        var pwd     = $('#pwd').val();
+//     function email_validator() {
+//         const pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+//         var email = $('#email').val();
 
-        if (pwd.length >= min) {
-            $('#passwordLength').css('display', 'none');
-            $('#pwd').removeClass('is-invalid');
-        }
-        else {
-            $('#passwordLength').css('display', 'block');
-            $('#pwd').addClass('is-invalid');
-        }
-    }
+//         if (pattern.test(email) && email != "") {
+//             $('#invalidEmail').css('display', 'none');
+//             $('#email').removeClass('is-invalid');
+//         } else {
+//             $('#invalidEmail').css('display', 'block');
+//             $('#email').addClass('is-invalid');
+//         }
+//     }
 
-    function password_length_validator() {
-        var pwd1    = $('#pwd').val();
-        var pwd2    = $('#pwdConfirm').val();
+//     function password_validator() {
+//         const min = 8;
+//         var pwd = $('#pwd').val();
 
-        if (pwd1 === pwd2) {
-            $('#passwordDifferent').css('display', 'none');
-            $('#pwdConfirm').removeClass('is-invalid');
-        }
-        else {
-            $('#passwordDifferent').css('display', 'block');
-            $('#pwdConfirm').addClass('is-invalid');
-        }
-    }
+//         if (pwd.length >= min) {
+//             $('#passwordLength').css('display', 'none');
+//             $('#pwd').removeClass('is-invalid');
+//         } else {
+//             $('#passwordLength').css('display', 'block');
+//             $('#pwd').addClass('is-invalid');
+//         }
+//     }
 
-});
+//     function password_length_validator() {
+//         var pwd1 = $('#pwd').val();
+//         var pwd2 = $('#pwdConfirm').val();
+
+//         if (pwd1 === pwd2 && pwd2 != "") {
+//             $('#passwordDifferent').css('display', 'none');
+//             $('#pwdConfirm').removeClass('is-invalid');
+//         } else {
+//             $('#passwordDifferent').css('display', 'block');
+//             $('#pwdConfirm').addClass('is-invalid');
+//         }
+//     }
+// });
+
+
+
+
+// ANDY's EXAMPLE CODE V
+
+
+
+
+// (function(){
+//     "use strict";
+
+//     var state = document.getElementById('email');
+
+//     document.addEventListener('DOMContentLoaded', function() {
+//         document.getElementById('register-button').addEventListener('submit', register);
+
+//         var button = document.getElementById('register-button');
+//         button.disabled = true;
+    
+//         state.addEventListener('change', function() {
+//             if(state === null){
+//                 button.disabled = true;
+//             }else{
+//                 button.disabled = false;
+//             }
+//         })
+//     })
+
+// })();
+
+// function register(event){
+//     event.preventDefault();
+//     console.log("hue");
+
+//     if(state.value === null){
+//         state.focus();
+//     }
+// }
