b558ac63b47f268f4984d0c89f70b98a0971be27
nickangmc <nickang.mc@hotmail.com>
2019-05-30 22:23:39 +1000

    WIP

--- public/scripts/dashboard/dash_reviews_script.js
+++ public/scripts/dashboard/dash_reviews_script.js
@@ -10,129 +10,133 @@ const myReviews = document.getElementById('my-reviews');
 const leftReviews = document.getElementById('left-reviews');
 
 getUser().then(function(){
-  // get all reviews about user
-  getReviewsFor(user._id).then(function(){
-    if (reviews === undefined || reviews.length == 0) {
-      myReviews.innerHTML = "<p>You have not received any reivews yet.</p>";
-    } else {
-      getReviewers().then(function(){
-        var reviews_content = "";
+	// get all reviews about user
+	getReviewsFor(user._id).then(function(){
+		// no reviews found
+		if (reviews === undefined || reviews.length == 0) {
+			myReviews.innerHTML = "<p>You have not received any reivews yet.</p>";
+		} else {
+			getReviewers().then(function(){
+				// to store all reviews' html structure
+				var reviews_content = "";
+				// render all reviews
+				reviews.forEach(function(review){
+					reviews_content += renderReview(review);
+				});
+				// write to html
+				myReviews.innerHTML = reviews_content;
+			});
+		}
+	});
 
-        // gets the first 5 reviews left for this user
-        reviews.forEach(function(review){
-          reviews_content += renderReview(review);
-        });
-
-        myReviews.innerHTML = reviews_content;
-      });
-    }
-  });
-
-  getReviewsLeft(user._id).then(function(){
-    if (reviewsLeft === undefined || reviewsLeft.length == 0) {
-      leftReviews.innerHTML = "<p>You have not reviewed any other users yet.</p>";
-    } else {
-      getReviewees().then(function(){
-        var reviews_content = '<table><tbody>';
-
-        // gets the first 5 reviews left for this user
-        reviewsLeft.forEach(function(review){
-          reviews_content += renderLeftReview(review);
-        });
-
-        reviews_content += '</table></tbody>';
-        leftReviews.innerHTML = reviews_content;
-      });
-    }
-  });
+	getReviewsLeft(user._id).then(function(){
+		// no reviews found
+		if (reviewsLeft === undefined || reviewsLeft.length == 0) {
+			leftReviews.innerHTML = "<p>You have not reviewed any other users yet.</p>";
+		} else {
+			getReviewees().then(function(){
+				// to store all reviews' html structure
+				var reviews_content = "";
+				// render all reviews
+				reviewsLeft.forEach(function(review){
+					reviews_content += renderLeftReview(review);
+				});
+				// write to html
+				leftReviews.innerHTML = reviews_content;
+			});
+		}
+	});
 
 });
 
 function renderReview(review){
-  review_content = "";
-  var reviewDate = new Date(review.datePosted);
+	// store all the html code
+	var review_content = "";
+	// preprocess all the required info
+	var datePosted = new Date(review.datePosted);
 
-  review_content +=
-    "<div class=\"class=review\">" +
-    "<h6 class=\"review-title\">" + review.title + "</h6>" +
-    "<img class=\"star-rating\" src=\"" + getStars(review.starRating) + "\">" +
-    "<div class=\"left-by\">Left by: <a href=\"" + profileURL + "?id=" + review.reviewer._id + "\">" +review.reviewer.name + "</a></div>" +
-    "<div class=\"date\">" + reviewDate.toLocaleDateString("en-AU", {year:"numeric", month:"short", day:"numeric"}) + "</div>" +
-    "<div class=\"review-content\">" + review.content + "</div>" +  
-    "</div>";
+	review_content += '<div class="review-container">' +
+							'<h6 class="review-title">' + review.title + '</h6>' +
+							'<div class="star-rating-container">' +
+								'<img class="star-rating" src="' + getStars(review.starRating) + '>' +
+							'</div>'	
+							'<div class="left-by">' +
+								'<a href="' + profileURL + '?id=' + review.reviewer._id + '>' + 'Left by: ' +review.reviewer.name + '</a>' + 
+								'<a class="date-posted">' + datePosted.toLocaleDateString("en-AU", {year:"numeric", month:"short", day:"numeric"}) + '</a>' +
+							'</div>' +
+							'<p class="review-content">' + review.content + '</p>' +  
+						'</div>';
 
-  return review_content;
+	return review_content;
 }
 
 function renderLeftReview(review) {
-  review_content = "";
-  var reviewDate = new Date(review.datePosted);
+	// store all the html code
+	var review_content = "";
+	// preprocess all the required info
+	var datePosted = new Date(review.datePosted);
 
-  review_content +=
-  "<tr class=\"class=review\">" +
-    "<td>" +
-      "<h6 class=\"review-title\">" + review.title + "</h6>" +
-      "<img class=\"star-rating\" src=\"" + getStars(review.starRating) + "\">" +
-      "<div class=\"left-for\">Left for: <a href=\"" + profileURL + "?id=" + review.reviewee._id + "\">" +
-      review.reviewee.name + "</a></div>" +
-      "<div class=\"date\">" +
-      reviewDate.toLocaleDateString("en-AU", {year:"numeric", month:"short", day:"numeric"}) + "</div>" +
-      "<div class=\"review-content\">" + review.content + "</div>" +
-    "</td>" +
-    "<td class=\"list-button\">" +
-      "<img src=\"images/dash/bin.png\" class=\"bin-image\">" +
-    "</td>" +
-  "</tr>";
+	review_content += '<div class="review-container">' +
+							'<h6 class="review-title">' + review.title + '</h6>' +
+							'<div class="star-rating-container">' +
+								'<img class="star-rating" src="' + getStars(review.starRating) + "\">" +
+							'</div>'	
+							'<div class="left-for">' + 
+								'<a href="' + profileURL + '?id=' + review.reviewee._id + '>' + 'Left for: ' + review.reviewee.name + '</a>' + 
+								'<a class="date-posted">' + datePosted.toLocaleDateString("en-AU", {year:"numeric", month:"short", day:"numeric"}) + '</a>' +
+							'</div>' +
+							'<p class="review-content">' + review.content + '</p>' +  
+						'</div>';
 
-  return review_content;
+	return review_content;
 }
 
 // returns all review written about given user
 function getReviewsFor(userId){
-  return new Promise(resolve => {
-      jQuery.get(reviewURL + "/userId/" + userId, function(data){
-        reviews = data;
-        resolve();
-      });
-  });
+	return new Promise(resolve => {
+		jQuery.get(reviewURL + "/userId/" + userId, function(data){
+			reviews = data;
+			resolve();
+		});
+	});
 }
 
 // returns all review left by a given user
 function getReviewsLeft(userId){
-  return new Promise(resolve => {
-      jQuery.get(reviewURL + "/leftUserId/" + userId, function(data){
-        reviewsLeft = data;
-        resolve();
-      });
-  });
+	return new Promise(resolve => {
+		jQuery.get(reviewURL + "/leftUserId/" + userId, function(data){
+			reviewsLeft = data;
+			resolve();
+		});
+	});
 }
 
 // given a review, will get the reviewer and add it to that review
 function getReviewer(review){
-  return new Promise(resolve => {
-    jQuery.get(userURL + "/id/" + review.leftById, function(user) {
-      resolve(review.reviewer = user);
-    });
-  });
+	return new Promise(resolve => {
+		jQuery.get(userURL + "/id/" + review.leftById, function(user) {
+		resolve(review.reviewer = user);
+		});
+	});
 }
 
 // add associated user to all reviews
 async function getReviewers(){
-  const promises = reviews.map(getReviewer);
-  await Promise.all(promises);
+	const promises = reviews.map(getReviewer);
+	await Promise.all(promises);
 }
 
 // given a review, will get the reviewee and add it to that review
 function getReviewee(review){
-  return new Promise(resolve => {
-    jQuery.get(userURL + "/id/" + review.userId, function(user) {
-      resolve(review.reviewee = user);
-    });
-  });
+	return new Promise(resolve => {
+		jQuery.get(userURL + "/id/" + review.userId, function(user) {
+		resolve(review.reviewee = user);
+		});
+	});
 }
 
 // add associated reviewee to all reviews
 async function getReviewees(){
-  const promises = reviewsLeft.map(getReviewee);
-  await Promise.all(promises);
+	const promises = reviewsLeft.map(getReviewee);
+	await Promise.all(promises);
 }
